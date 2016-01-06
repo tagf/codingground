71069badce393d5b64a481413efd8745f239c9b0
tagf <tagf@users.noreply.github.com>
2016-01-06 09:41:09 +0300

    Created by Coding Ground Client

---
+++ New Project/smart_ptr.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+template <class Type>
+class SmartPointer {
+private:
+    Type *pointer;
+public:
+    SmartPointer(Type* p): pointer(p) {};
+    operator Type*() { return pointer; };
+    Type *operator->()  {
+        if (!pointer) {
+            pointer = new Type();
+            std::cerr << "Bad pointer!" << std::endl;
+        }
+        return pointer; 
+    };
+    std::ptrdiff_t operator-(SmartPointer<Type> p) {
+        return pointer - p;
+    }
+    std::ptrdiff_t operator-(Type* /* void*? */ p) {
+        return pointer - p;
+    }
+};
+
+class Foo {
+private:
+    int a, b;
+public:
+    Foo(): a(0), b(0) {};
+    Foo(int a, int b): a(a), b(b) {};
+    int Sum() { return a + b; };
+};
+
+int main(int argc, char *argv[])
+{
+    SmartPointer<Foo> sp1(new Foo(2,2));
+    SmartPointer<Foo> sp2(NULL);
+    std::cout << sp1->Sum() << std::endl; 
+     std::cout << sp2->Sum() << std::endl; 
+   return 0;
+}
+
